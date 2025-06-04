93eeb90d859e3bb2aeef34a1698fa416e3ca6bd8
Samuel Huang <hi@sgh.ng>
2025-06-04 15:48:02 -0400

    nvim: fix vue typescript support after mason v2

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -6,21 +6,42 @@ return {
 		dependencies = "neovim/nvim-lspconfig",
 		event = "VeryLazy",
 		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"cssls",
-					"html",
-					"jsonls",
-					"lemminx", -- xml
-					"lua_ls",
-					"marksman",
-					"ruff",
-					"taplo",
-					"ts_ls",
-					"unocss",
-					"vimls",
-					"vue_ls",
+			require("mason-lspconfig").setup(
+				---@type MasonLspconfigSettings
+				{
+					ensure_installed = {
+						"air", -- R LSP and Formatter
+						"cssls",
+						"html",
+						"jsonls",
+						"lemminx", -- xml
+						"lua_ls",
+						"marksman",
+						"ruff",
+						"tailwindcss",
+						"taplo", -- toml
+						"ts_ls",
+						"unocss",
+						"vimls",
+						"vue_ls",
+					},
+				}
+			)
+
+			--- Additional configuration for Vue.js support
+			local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server")
+			local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+			vim.lsp.config("ts_ls", {
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vue_plugin_path,
+							languages = { "vue" },
+						},
+					},
 				},
+				filetypes = { "typescript", "javascript", "vue" },
 			})
 		end,
 	},
