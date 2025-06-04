0f983060bbaec10078a61158e194599a40615a4d
Samuel Huang <hi@sgh.ng>
2025-06-03 20:09:46 -0400

    nvim: introduce trouble.nvim

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -55,8 +55,7 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		event = "LspAttach",
-		---@module "lspsaga"
-		---@type LspsagaConfig
+		cond = false,
 		opts = { lightbulb = { enable = false } }, -- takes up to much space
 	},
 	{
@@ -77,6 +76,43 @@ return {
 			"AerialOpen",
 			"AerialOpenAll",
 			"AerialToggle",
+		},
+		opts = {}, -- required
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+		keys = {
+			{
+				"<Leader>xx",
+				"<Cmd>Trouble diagnostics toggle<CR>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<Leader>xX",
+				"<Cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<Leader>cs",
+				"<Cmd>Trouble symbols toggle focus=false<CR>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<Leader>cl",
+				"<Cmd>Trouble lsp toggle focus=false win.position=right<CR>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<Leader>xL",
+				"<Cmd>Trouble loclist toggle<CR>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<Leader>xQ",
+				"<Cmd>Trouble qflist toggle<CR>",
+				desc = "Quickfix List (Trouble)",
+			},
 		},
 		opts = {}, -- required
 	},
@@ -148,4 +184,5 @@ return {
 		end,
 	},
 	{ "echasnovski/mini.animate", cond = not vim.g.neovide, opts = {} },
+	"Bekaboo/deadcolumn.nvim",
 }
