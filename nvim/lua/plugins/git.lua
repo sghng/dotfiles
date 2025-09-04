780214de2fb0162d47ea7b0229b1416e5e6f8baf
Samuel Huang <hi@sgh.ng>
2025-09-04 16:14:57 -0400

    nvim: updated git configs

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,26 +1,44 @@
 ---@type LazySpec
 return {
-	{ "tpope/vim-fugitive", event = "VeryLazy" }, -- too many commands
-	{
-		"airblade/vim-gitgutter",
-		event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			vim.cmd("GitGutterLineNrHighlightsEnable")
-		end,
-	},
 	{
 		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-			"LazyGitLog",
-		},
-		keys = { { "<Leader>g", vim.cmd.LazyGit, desc = "Lazy[g]it" } },
+		cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile" },
+		keys = { { "<Leader>gg", vim.cmd.LazyGit, desc = "Lazy[g]it" } },
 		config = function()
 			vim.g.lazygit_floating_window_scaling_factor = 0.9
 		end,
+	},
+	{
+		-- NOTE: this plugin also supports some hunk actions
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		cmd = "Gitsigns",
+		keys = {
+			{ "<Leader>]h", "<Cmd>Gitsigns next_hunk<CR>", desc = "Next [h]unk" },
+			{ "<Leader>[h", "<Cmd>Gitsigns prev_hunk<CR>", desc = "Previous [h]unk" },
+			{ "<Leader>gb", "<Cmd>Gitsigns blame<CR>", desc = "[g]it [b]lame" },
+			{ "<Leader>gh", "<Cmd>Gitsigns toggle_linehl<CR>", desc = "toggle [g]it hunk [h]ighlight" },
+			{ "<Leader>gd", "<Cmd>Gitsigns diffthis<CR>", desc = "[g]it [d]iff this buffer" },
+		},
+		---@module "gitsigns"
+		---@type Gitsigns.Config
+		---@diagnostic disable-next-line:missing-fields
+		opts = {
+			current_line_blame = true,
+			preview_config = { border = "rounded" },
+			sign_priority = 10, -- push signs to the left
+		},
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<Leader>gn", "<Cmd>Neogit kind=floating<CR>", desc = "open [n]eogit popup" },
+		},
 	},
 }
