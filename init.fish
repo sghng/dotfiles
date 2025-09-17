9678fa415d6066b9a5faf6b5c6d4d9548c3cc40c
Samuel Huang <hi@sgh.ng>
2025-09-16 22:16:37 -0400

    init: improvements

--- init.fish
+++ init.fish
@@ -2,7 +2,7 @@
 
 set -l d (realpath (dirname (status -f)))
 
-for c in .zshrc .zprofile .tmux.conf .wezterm.lua
+for c in .zshrc .zprofile .tmux.conf .wezterm.lua cspell.config.yaml
     ln -sf $d/$c ~
 end
 
@@ -10,14 +10,14 @@ for c in ghostty kitty alacritty rio nvim neovide starship.toml
     ln -sf $d/$c ~/.config/
 end
 
-set -l VALE_CONFIG_PATH "~/Library/Application Support/vale/"
+set -l VALE_CONFIG_PATH "$HOME/Library/Application Support/vale/"
 cd $VALE_CONFIG_PATH
 ln -sf $d/.vale.ini .
 vale sync # must be executed inside VALE_CONFIG_PATH
 
 cd ~/.config/rio
 curl -L https://github.com/libretro/slang-shaders/archive/master.tar.gz |
-    tar -xz --strip-components=1
+    tar -xz --strip-components=1 -C shaders
 
 touch ~/.hushlogin
 
