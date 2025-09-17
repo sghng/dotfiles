0e0cbe0b8859792e441e9683f40d6af2fe8b3ef2
Samuel Huang <hi@sgh.ng>
2025-09-16 22:14:22 -0400

    fish: a bunch of improvements including aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set fish_greeting ""
+    cursor-agent shell-integration fish
+    starship init fish | source
 end
-
-set fish_greeting ""
 
 # PATH
 fish_add_path /opt/homebrew/bin
@@ -12,15 +12,33 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.local/bin
 
-starship init fish | source
-
 # ENV
 set -x EDITOR nvim
+set -x BAT_THEME gruvbox-dark
+set -x MANPAGER 'nvim -M +Man!'
 set -x DOTS $HOME/dev/dotfiles
 set -x UV_PYTHON 3.13
+set -x CSPELL_DEFAULT_CONFIG_PATH $HOME/.config/cspell.config.yaml
 
 # ABBR
 abbr -a vi nvim
-abbr -a less "less --mouse"
+abbr -a ls lsd
+abbr -a ll lsd -l
+abbr -a la lsd -a
+abbr -a du dust
+abbr -a df duf
+abbr -a cat bat
 abbr -a dots "cd $DOTS"
 abbr -a t "tmux attach -t base; or tmux new -s base"
+
+# FUNC
+function vast # nvim as a pager
+    nvim -M +"nnoremap q :qa!<CR>" +"set nonumber norelativenumber signcolumn=no" -
+end
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+set -gx MAMBA_EXE /opt/homebrew/opt/micromamba/bin/mamba
+set -gx MAMBA_ROOT_PREFIX /Users/sghuang/mamba
+$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
