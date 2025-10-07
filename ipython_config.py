35149c074dc454fd79c688ad2915c9b6cc57a985
Samuel Huang <hi@sgh.ng>
2025-10-07 13:34:53 -0400

    ipython: move config out to root dir

--- ipython_config.py
+++ ipython_config.py
@@ -1,4 +1,5 @@
-c = c  # noqa: F821
+# ruff: noqa: F821
+c = c  # ty: ignore[unresolved-reference]
 c.InteractiveShell.ast_node_interactivity = "all"
 c.InteractiveShellApp.exec_lines = [
     "%config InlineBackend.figure_format = 'retina'",
