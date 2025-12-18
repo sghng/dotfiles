e20152c1296feffc2822690ec606a9caad13cb2a
Samuel Huang <hi@sgh.ng>
2025-12-18 15:38:52 -0500

    nvim: migrate tree-sitter to main branch

--- nvim/lua/mods/treesitter.lua
+++ nvim/lua/mods/treesitter.lua
@@ -2,95 +2,15 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = { "brew install tree-sitter tree-sitter-cli", ":TSUpdate" },
 		event = { "BufReadPost", "BufNewFile" },
-		---@type TSConfig
-		---@diagnostic disable-next-line: missing-fields
-		opts = {
-			auto_install = true,
-			highlight = {
-				enable = true,
-				disable = { "csv", "tsv" }, -- builtin highlighting is better
-			},
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "gnn",
-					node_incremental = "grn",
-					scope_incremental = "grc",
-					node_decremental = "grm",
-				},
-			},
-			-- embedded languages may not be recognized from filetypes
-			ensure_installed = { "javascript", "latex", "regex", "typescript" },
-		},
-		-- manual setup required
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
-	{
-		-- prefer vim.lsp for such features
-		"nvim-treesitter/nvim-treesitter-refactor",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" }, -- for definition highlights
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("nvim-treesitter.configs").setup({
-				refactor = {
-					highlight_definitions = { enable = true },
-					highligh_current_scope = { enable = true },
-				},
-			})
-			vim.opt.updatetime = 500
-		end,
+		-- TODO: enable tree sitter features
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main", -- TODO: remove this once main is released
 		dependencies = "nvim-treesitter/nvim-treesitter",
-		keys = function()
-			local t = {}
-			for _, k in ipairs({ "af", "if", "ac", "ic", "as" }) do
-				table.insert(t, { k, mode = "v" })
-			end
-			return t
-		end,
 		---@module "nvim-treesitter-textobjects"
-		opts = {
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = {
-							query = "@function.outer",
-							desc = "Select [a] [f]unction",
-						},
-						["if"] = {
-							query = "@function.inner",
-							desc = "Select [i]nner of a [f]unction",
-						},
-						["ac"] = {
-							query = "@class.outer",
-							desc = "Select [a] [c]lass",
-						},
-						["ic"] = {
-							query = "@class.inner",
-							desc = "Select [i]nner of a [c]lass",
-						},
-						["as"] = {
-							query = "@local.scope",
-							desc = "Select [a] [s]cope",
-						},
-					},
-					include_surrounding_whitespace = true,
-				},
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
+		-- TODO: define objects
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
