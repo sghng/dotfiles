7abe6aad7461521d40ca5421ae851bf75d7d9748
Samuel Huang <hi@sgh.ng>
2025-12-29 22:21:06 -0500

    dns: refactored with single redirects

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -4,7 +4,16 @@
 var REG_NONE = NewRegistrar("none");
 var DSP_CLOUDFLARE = NewDnsProvider("cloudflare", undefined, {
 	manage_redirects: true,
+	manage_single_redirects: true,
 });
+
+var CF_PROXY_ON = { cloudflare_proxy: "on" };
+var REDIRECT_TO_PRIMARY_DOMAIN = CF_SINGLE_REDIRECT(
+	"redirect to sghuang.com",
+	301,
+	"*sgh.ng/*",
+	"https://$1sghuang.com/$2",
+);
 
 /* Primary domain */
 
@@ -23,13 +32,12 @@ D(
 	CNAME("naive", "cname.vercel-dns.com."),
 	CNAME("typ2docx", "typ2docx.onrender.com."),
 	// proxy needed for Obsidian
-	CNAME("notes", "publish-main.obsidian.md.", { cloudflare_proxy: "on" }),
+	CNAME("notes", "publish-main.obsidian.md.", CF_PROXY_ON),
+	A("jellyfin", "129.74.246.41"),
 
 	// redirections
-	A("in", "192.0.2.1", { cloudflare_proxy: "on" }),
-	A("git", "192.0.2.1", { cloudflare_proxy: "on" }),
-	// ideally we should use single redirection, but DNSControl hasn't implmeneted
-	// regex replacement in conversion mode
+	A("in", "192.0.2.1", CF_PROXY_ON),
+	A("git", "192.0.2.1", CF_PROXY_ON),
 	CF_REDIRECT("in.sghuang.com", "https://linkedin.com/in/sghng"),
 	CF_REDIRECT("git.sghuang.com", "https://github.com/sghng"),
 );
@@ -41,16 +49,15 @@ D(
 	REG_NONE,
 	DnsProvider(DSP_CLOUDFLARE),
 	// proxy all domains
-	A("@", "192.0.2.1", { cloudflare_proxy: "on" }),
-	A("*", "192.0.2.1", { cloudflare_proxy: "on" }),
+	A("@", "192.0.2.1", CF_PROXY_ON),
+	A("*", "192.0.2.1", CF_PROXY_ON),
 	// email
 	MX("@", 10, "mx01.mail.icloud.com."),
 	MX("@", 10, "mx02.mail.icloud.com."),
 	CNAME("sig1._domainkey", "sig1.dkim.sgh.ng.at.icloudmailadmin.com."),
 	TXT("@", "apple-domain=3ewMGQM5mVlNnFbt"),
 	TXT("@", "v=spf1 include:icloud.com ~all"),
-	// regex replacement is not supported in conversion mode yet
-	CF_REDIRECT("*sgh.ng/*", "https://$1sghuang.com/$2"),
+	REDIRECT_TO_PRIMARY_DOMAIN,
 );
 
 /* Other shorthands */
@@ -60,10 +67,9 @@ D(
 	REG_NONE,
 	DnsProvider(DSP_CLOUDFLARE),
 	// proxy all domains
-	A("@", "192.0.2.1", { cloudflare_proxy: "on" }),
-	A("*", "192.0.2.1", { cloudflare_proxy: "on" }),
-	// regex replacement is not supported in conversion mode yet
-	CF_REDIRECT("*sghua.ng/*", "https://$1sghuang.com/$2"),
+	A("@", "192.0.2.1", CF_PROXY_ON),
+	A("*", "192.0.2.1", CF_PROXY_ON),
+	REDIRECT_TO_PRIMARY_DOMAIN,
 );
 
 D(
@@ -71,8 +77,7 @@ D(
 	REG_NONE,
 	DnsProvider(DSP_CLOUDFLARE),
 	// proxy all domains
-	A("@", "192.0.2.1", { cloudflare_proxy: "on" }),
-	A("*", "192.0.2.1", { cloudflare_proxy: "on" }),
-	// regex replacement is not supported in conversion mode yet
-	CF_REDIRECT("*sghng.com/*", "https://$1sghuang.com/$2"),
+	A("@", "192.0.2.1", CF_PROXY_ON),
+	A("*", "192.0.2.1", CF_PROXY_ON),
+	REDIRECT_TO_PRIMARY_DOMAIN,
 );
