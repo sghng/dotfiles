32e8d82af55a91c47a69053124f24b9d72f0a753
Samuel Huang <hi@sgh.ng>
2026-05-27 21:58:43 +0800

    misc: rename user name to sghng

--- fish/config.fish
+++ fish/config.fish
@@ -56,6 +56,9 @@ set -x CSPELL_DEFAULT_CONFIG_PATH $HOME/.config/cspell.config.yaml
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba shell init' !!
 set -gx MAMBA_EXE /opt/homebrew/opt/micromamba/bin/mamba
-set -gx MAMBA_ROOT_PREFIX /Users/sghuang/mamba
+set -gx MAMBA_ROOT_PREFIX /Users/sghng/mamba
 $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 # <<< mamba initialize <<<
+
+# OpenClaw Completion
+source "/Users/sghng/.openclaw/completions/openclaw.fish"
